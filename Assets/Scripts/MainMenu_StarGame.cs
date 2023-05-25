e7a5f49392da492a4b4111cda504d3038e1c1230
nooboclock <robinson12035@gmail.com>
2023-05-25 20:44:19 +0200

    Revert "Merge branch 'master' into alex"
    This reverts commit feaa619020659b1be2a231dd1fd63490ce15c2eb, reversing
    changes made to eacc4d6809075e0d104a64256237ed34d909db98.

--- Assets/Scripts/MainMenu_StarGame.cs
+++ Assets/Scripts/MainMenu_StarGame.cs
@@ -5,43 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu_StarGame : MonoBehaviour
 {
-    public GameObject shadow;
-    public Animator transition;
-
-    public void Awake()
-    {
-        StartCoroutine(MenuStartup());
-    }
-
     public void StartGame()
     {
-        StartCoroutine(LoadLevel());
-    }
-
-    public void QuitGame()
-    {
-        StartCoroutine(Quit());
-    }
-
-    public IEnumerator LoadLevel()
-    {
-        transition.Play("Transition out");
-        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(1);
-    }
-
-    public IEnumerator Quit()
-    {
-        transition.Play("Transition out");
-        yield return new WaitForSeconds(1);
-        Application.Quit();
-    }
-
-    public IEnumerator MenuStartup()
-    {
-        print("bada bing");
-        yield return new WaitForSeconds(2);
-        shadow.SetActive(false);
-        print("bada boom");
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float distanceToMove = 1f;
     [SerializeField] private LayerMask boxLayerMask;
 
-    [SerializeField] private Vector2 swipeStartPos, swipeEndPos, currentSwipe;
-    [SerializeField] private string swipeDirection;
-    [SerializeField] private bool canMove = true;
-
     private void Update()
     {
         if (!PauseMenu.isPaused)
@@ -17,32 +13,6 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.S)) MoveDown();
             if (Input.GetKeyDown(KeyCode.A)) MoveLeft();
             if (Input.GetKeyDown(KeyCode.D)) MoveRight();
-        }
-
-        DetectSwipeInput();
-        if (Input.touches.Length > 0)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Ended && canMove)
-            {
-                print("swiped");
-                canMove = false;
-                if (swipeDirection == "Up")
-                {
-                    MoveUp();
-                }
-                else if (swipeDirection == "Down")
-                {
-                    MoveDown();
-                }
-                else if (swipeDirection == "Left")
-                {
-                    MoveLeft();
-                }
-                else if (swipeDirection == "Right")
-                {
-                    MoveRight();
-                }
-            }
         }
     }
 
@@ -68,7 +38,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void TryMove(Vector3 direction)
     {
-        canMove = true;
         RaycastHit hit;
         if (Physics.BoxCast(transform.position, Vector3.one * 0.4f, direction, out hit, Quaternion.identity, distanceToMove, boxLayerMask))
         {
@@ -80,52 +49,5 @@ public class PlayerMovement : MonoBehaviour
             else if (slidingBox != null) slidingBox.MoveBox(direction);
         }
         transform.position += direction * distanceToMove;
-    }
-
-    private void DetectSwipeInput()
-    {
-        if (Input.touches.Length > 0)
-        {
-            Touch t = Input.GetTouch(0);
-
-            if (t.phase == TouchPhase.Began)
-            {
-                swipeStartPos = new Vector2(t.position.x, t.position.y);
-            }
-
-            if (t.phase == TouchPhase.Ended)
-            {
-                swipeEndPos = new Vector2(t.position.x, t.position.y);
-                currentSwipe = new Vector3(swipeEndPos.x - swipeStartPos.x, swipeEndPos.y - swipeStartPos.y);
-
-                if (currentSwipe.magnitude < 40)
-                {
-                    return;
-                }
-            }
-
-            currentSwipe.Normalize();
-
-            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-            {
-                swipeDirection = "Up";
-            }
-            else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-            {
-                swipeDirection = "Down";
-            }
-            else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-            {
-                swipeDirection = "Left";
-            }
-            else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-            {
-                swipeDirection = "Right";
-            }
-            else
-            {
-                swipeDirection = "None";
-            }
-        }
     }
 }
