e95f4ea7cd0512a6a28b6dd555c0179ef72b1029
nooboclock <robinson12035@gmail.com>
2023-05-26 22:58:08 +0200

    particle

--- Assets/Scripts/BoxMovement.cs
+++ Assets/Scripts/BoxMovement.cs
@@ -7,12 +7,29 @@ public class BoxMovement : MonoBehaviour
     [SerializeField] private float distanceToMove = 1f;
     [SerializeField] private LayerMask boxLayerMask;
 
+    private ParticleSystem particles;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
     public void MoveBox(Vector3 moveDirection)
     {
         RaycastHit hit;
         if (!Physics.BoxCast(transform.position, Vector3.one * 0.4f, moveDirection, out hit, Quaternion.identity, distanceToMove, boxLayerMask))
         {
             transform.position += moveDirection * distanceToMove;
+
+            if(particles != null)
+            {
+                particles.Play();
+            }
+
+            else
+            {
+                Debug.Log("Missing particles");
+            }
 
             if(Camera.main.GetComponent<ScreenShake>() != null)
             {
